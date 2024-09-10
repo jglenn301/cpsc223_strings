a4db58d7597c249d9df4241635cbcdf983132914
Glenn James <jrg94@zoo.cs.yale.edu>
2024-09-10 16:19:22 -0400

    fixed!

--- strings.c
+++ strings.c
@@ -4,13 +4,13 @@
 int main()
 {
   char start[] = "IST";
-  char end[] = {'E', 'W', 'R'}; // missing /0 causes unpredictable behavior
+  char end[] = {'E', 'W', 'R', '\0'}; // or just use string initializer!
   char tour[8];
   strcpy(tour, start);
   strcat(tour, "-");
   strcat(tour, end);
 
-  printf("tour %s\n", tour); // IST-EWRIST (on the Zoo today, at least)
+  printf("tour %s\n", tour); // IST-EWR, as expected
   printf("end %s\n", end);
   printf("start %s\n", start);
 }
